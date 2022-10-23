2d4ad0f5044ddd38d719f7a89bad0736f96742b9
Sachit Ramesh <quantechlxxi.corp@gmail.com>
2022-10-23 12:11:40 +0530

    Update takeoff.py

--- autopilot/takeoff.py
+++ autopilot/takeoff.py
@@ -5,4 +5,6 @@ print('Shift now')
 time.sleep(20)
 pyautogui.scroll(2500) 
 
-pyautogui.mouseUp()
+pyautogui.mouseUp() #This differs from plane to plane, while lighter planes like the MiG-21 would take off with this command, heavier ones would need something like this
+
+# pyautogui.mouseUp(x=850,y=300) #To get a proper push from the ground incase of heavier planes
