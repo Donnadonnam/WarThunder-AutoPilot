e6097e2ab0ff3d540b8a14a89b3fd7c433b613a6
Sachit Ramesh <quantechlxxi.corp@gmail.com>
2022-10-25 12:04:22 +0530

    Create targetting.py

---
+++ autopilot/targetting.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+import pydirectinput
+
+def find():
+    while True:
+        try:  
+            time.sleep(20) 
+            x,y = pyautogui.locateCenterOnScreen('Untitled.png',confidence=0.5,grayscale=True,region=(615,152,700,500))
+            pydirectinput.click(x, y,button='left')
+            
+            print(x,y)
+        except Exception as e:
+            print(e)          
+find()
